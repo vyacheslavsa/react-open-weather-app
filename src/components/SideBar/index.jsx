3eb429192c17bba44d7f9f8cce98aa3ad77c889c
Vyacheslav <viacheslav.saiapin@gmail.com>
2022-12-10 18:15:18 +0300

    fix side bar

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -4,33 +4,33 @@ import cs from "classnames";
 import {ReactComponent as IconBack} from "../../../src/image/back.svg";
 import {ReactComponent as SettingsIcon} from "../../image/settings_FILL0_wght300_GRAD200_opsz48.svg";
 import {Link} from "react-router-dom";
-import {allLanguage} from "../../constans";
-import {useSelector} from "react-redux";
 
 const SideBar = ({onClose, state}) => {
-    const currentLang = useSelector(state => state?.data?.interfaceLanguage)
-    const localStoreWeather = JSON.parse(localStorage.getItem('CURRENT_LANG'));
-    const lang = localStoreWeather || currentLang;
+    // const currentLang = useSelector(state => state?.data?.interfaceLanguage)
+    // const localStoreWeather = JSON.parse(localStorage.getItem('CURRENT_LANG'));
+    // const lang = localStoreWeather || currentLang;
+
+    //{allLanguage[lang].sideBar.back}
 
     return (
         <div className={cs(styles.root, {[styles.openBar]: state})}>
             <div className={styles.header}>
                 <div onClick={() => onClose()} className={styles.backBlock}>
                     <IconBack/>
-                    <span>{allLanguage[lang].sideBar.back}</span>
+                    <span>Back</span>
                 </div>
                 <div style={{
                     display: 'flex',
                     alignItems: 'center',
                     justifyContent: 'center'
-                }}>{allLanguage[lang].sideBar.menu}</div>
+                }}>Menu</div>
             </div>
             <div className={styles.content} onClick={() => onClose()}>
                 <Link to='/settings'>
                     <div className={styles.item}>
                         <div className={styles.itemContent}>
                             <SettingsIcon/>
-                            <span>{allLanguage[lang].sideBar.settings}</span>
+                            <span>Settings</span>
                         </div>
                     </div>
                 </Link>

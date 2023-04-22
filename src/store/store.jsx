5d9ea91700d4e67ef1ede545b348888cce720f02
viacheslav <viacheslav.saiapin@gmail.com>
2023-04-22 21:06:30 +0300

    remove storybook

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,13 +1,14 @@
-import {combineReducers, createStore, applyMiddleware} from "redux";
-import {reduser} from '../redusers/reduser'
-import {composeWithDevTools} from 'redux-devtools-extension';
-import thunk from 'redux-thunk'
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import { reduser } from "../redusers/reduser";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
 
-const combine = combineReducers({//combineReducers функция которая объединяет редюсеры
-    data: reduser
+const combine = combineReducers({
+  //combineReducers функция которая объединяет редюсеры
+  data: reduser,
 });
 
 export const store = createStore(
-    combine,
-    composeWithDevTools(applyMiddleware(thunk))//привязываем redux-dev-tools
+  combine,
+  composeWithDevTools(applyMiddleware(thunk)) //привязываем redux-dev-tools
 );
